e6dd2b20c6211b5f2729d0c77264513cbea1c606
Kosisochukwu Azubogu <azuboguko@gmail.com>
2026-05-05 09:11:08 +0100

    modify: Correct API URL to match URL for production environments

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://api.eatwella.ng",
+  baseURL: "https://api.eatwella.ng/api",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
