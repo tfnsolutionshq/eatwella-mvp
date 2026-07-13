8f6134287696ea57d22672bdf1bc91fbedfb8a41
Azubogu Kosiso <azuboguko@gmail.com>
2026-07-13 13:33:10 +0100

    Update Dashboard.jsx

--- src/Pages/Admin/Dashboard.jsx
+++ src/Pages/Admin/Dashboard.jsx
@@ -27,7 +27,7 @@ const Dashboard = () => {
           api.get("/admin/analytics/top-menus"),
           api.get("/admin/analytics/daily-sales"),
         ]);
-        console.log("this is it: ", summaryRes, topMenusRes, dailySalesRes);
+        console.log("right here: ", dailySalesRes.data.data);
         setSummary(summaryRes.data);
         setTopMenus(topMenusRes.data);
         setDailySales(dailySalesRes.data);
